a4229f22dd734fde438ab5b6ba6b00029e5cd9fa
Felipe Diniz <felipeborgesdiniz@gmail.com>
2026-08-20 21:01:18 -0300

    Add files via upload

---
+++ AULA_PYTHON/exercicio02.py
@@ -0,0 +1,19 @@
+#“A diferença entre um número e sua terça parte é 20.”
+#Qual é esse número?
+
+
+# A diferença entre um número e sua terça parte é 20.
+# x - (x / 3) = 20
+# (3x - x) / 3 = 20
+# 2x / 3 = 20
+# 2x = 60
+# x = 30
+
+x = 30
+terceira_parte = x/3
+dobro = x*2
+diferença_terceira = x - terceira_parte
+
+print(f"O numero e {x}")
+print(f"O dobro do numero e {dobro}")
+print (f"A diferenca do numero com a terceira parte e {diferença_terceira}")
