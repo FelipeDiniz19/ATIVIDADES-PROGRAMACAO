6f1198122a0b4d8e6161872546cf98707f7e0b34
Felipe Diniz <felipeborgesdiniz@gmail.com>
2026-08-27 21:34:54 -0300

    Add files via upload

---
+++ Explicação_if_else/explicação_if.py
@@ -0,0 +1,9 @@
+# O comando `if` permite que o programa execute um bloco de código apenas se uma determinada condição for verdadeira.
+
+print("--- Exemplo 1: Simples if ---")
+idade = 20
+print(f"Idade informada: {idade}")
+
+if idade >= 18:
+    print("Acesso permitido. Você é maior de idade.")
+
