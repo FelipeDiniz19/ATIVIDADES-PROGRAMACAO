7884106aa97d6776b195e32578bafd9dbc3cfcf2
Felipe Diniz <felipeborgesdiniz@gmail.com>
2026-09-10 18:42:20 -0300

    Add files via upload

---
+++ Lógica_exercicios/exercicio32.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite um numero: "))
+
+if n1 >= 10 and n1 <= 20:
+    print("DENTRO")
+
+else:
+    print("FORA")
---
+++ Lógica_exercicios/exercicio33.py
@@ -0,0 +1,28 @@
+print ("---------------DIA DA SEMANA---------------")
+
+
+n1 = int(input("Digite o numero e veja qual dia da semana: "))
+
+if n1 == 1:
+    print("SEGUNDA-FEIRA")
+
+elif n1 == 2:
+    print("TERCA-FEIRA")
+
+elif n1 == 3:
+    print("QUARTA-FEIRA")
+
+elif n1 == 4:
+    print("QUINTA-FEIRA")
+
+elif n1 == 5:
+    print("SEXTA-FEIRA")
+
+elif n1 == 6:
+    print("SABADO")
+
+elif n1 == 7:
+    print("DOMINGO")
+
+else:
+    print("OPCAO INVALIDA")
